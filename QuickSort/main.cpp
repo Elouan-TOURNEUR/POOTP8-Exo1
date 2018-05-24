ec862c0d6643c4211a4c483e140d855178532801
Elouan Tourneur <37573334+Elouan-TOURNEUR@users.noreply.github.com>
2018-05-24 12:16:43 +0200

    exo1 et 2

---
+++ QuickSort/main.cpp
@@ -0,0 +1,18 @@
+//#include "quicksort.cpp"
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//using namespace std;
+
+
+//int main()
+//{
+//    vector <int> tt = {2, 7, 8, 9, 1, 9, 2, 8, 6, 4, 2, 9, 1, 8, 3, 4} ;
+//    quickSort(tt, 0, 15);
+//    for (unsigned i = 0 ; i < tt.size() ; ++i)
+//    {
+//        cout << tt[i];
+//    }
+//}
+
+//template <typename>
